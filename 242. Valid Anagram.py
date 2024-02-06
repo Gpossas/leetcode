788e234277428fe28cac3f88f098cdab6dfa6631
Gpossas <possasg@gmail.com>
2024-02-06 09:45:00 -0300

    add

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -12,3 +12,19 @@ class Solution:
                 return False
             char_map[char] -= 1
         return True
+
+ALPHABET_LETTERS = 26
+class SolutionNoMap:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        frequency = [0] * ALPHABET_LETTERS
+        for char in s:
+            frequency[ord(char) - ord('a')] += 1
+        
+        for char in t:
+            frequency[ord(char) - ord('a')] -= 1
+            if frequency[ord(char) - ord('a')] < 0:
+                return False
+        return True
